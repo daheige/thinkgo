72ddd601e8d0e79c0db46d4490067b01df4c7e10
daheige <zhuwei313@hotmail.com>
2018-06-03 17:42:55 +0800

    通用协程池任务执行器实现

---
+++ GoPool/gopool_test.go
@@ -0,0 +1,69 @@
+package GoPool
+
+import (
+	"fmt"
+	"testing"
+)
+
+type Calc struct {
+	in chan int
+}
+
+func (this *Calc) Run() (interface{}, error) {
+	sum := 0
+	for i := range this.in {
+		fmt.Printf("开始执行第%d次求和\n", i)
+		sum += i
+		fmt.Printf("当前%d累加的sum和: %d\n", i, sum)
+	}
+
+	// fmt.Println(sum)
+	return sum, nil
+}
+
+//退出获取结果
+func (this *Calc) Quit(_ *Pool, res interface{}, _ error) {
+	fmt.Println("get sum", res)
+}
+
+func AtQuit(_ *Pool, res interface{}, _ error) {
+	fmt.Println("get sum", res)
+}
+
+func Test_goroutine_pool(t *testing.T) {
+	intChan := make(chan int)
+	calc := Calc{intChan}
+
+	//创建3个goroutine pool
+	pool := FromRunner(3, &calc)
+	//pool := NewPool(3, calc.Run, AtQuit)
+	pool.Keepalive() //让3个goroutine保持激活状态
+	fmt.Println("worker number:", pool.Count())
+
+	//依次执行1048575次累加
+	for i := 0; i < 1024*1024; i++ {
+		intChan <- i
+	}
+	pool.AddExecutor() //创建pool执行器
+	fmt.Println("worker number:", pool.Count())
+	close(intChan)
+	pool.WaitAllQuit() //等待所有的goroutine执行完毕
+
+	fmt.Println("worker number:", pool.Count())
+	t.Log("success")
+}
+
+/**
+time go test -v
+当前1048575累加的sum和: 184003456399
+get sum 184003456399
+worker number: 0
+--- PASS: Test_goroutine_pool (17.18s)
+	gopool_test.go:53: success
+PASS
+ok  	thinkgo/GoPool	17.186s
+
+real	0m17.554s
+user	0m5.174s
+sys	0m5.701s
+*/
---
+++ GoPool/goroutinePool.go
@@ -0,0 +1,142 @@
+//通用协程池任务执行器实现
+package GoPool
+
+import (
+	"sync"
+	"sync/atomic"
+
+	"time"
+
+	"github.com/daheige/thinkgo/common"
+)
+
+/*demo
+eg:
+type Calc struct {
+    in chan int
+}
+func (this *Calc) Run() (interface{}, error) {
+    sum := 0
+    for i := range this.in {
+        sum += i
+    }
+    return sum, nil
+}
+func (this *Calc) Quit(_ *GoPool.Pool, res interface{}, _ error) {
+    fmt.Println("get sum", res)
+}
+func AtQuit(_ *GoPool.Pool, res interface{}, _ error) {
+    fmt.Println("get sum", res)
+}
+func main() {
+    intChan := make(chan int)
+    calc := Calc{intChan}
+    pool := GoPool.FromRunner(3, &calc)
+    //pool := GoPool.NewPool(3, calc.Run, AtQuit)
+    pool.Keepalive()
+    fmt.Println("worker number:", pool.Count())
+    for i := 0; i < 1024*1024; i++ {
+        intChan <- i
+    }
+    pool.AddExecutor()
+    fmt.Println("worker number:", pool.Count())
+    close(intChan)
+    pool.WaitAllQuit()
+    fmt.Println("worker number:", pool.Count())
+}
+*/
+
+type Runner interface {
+	Run() (interface{}, error)
+	Quit(*Pool, interface{}, error)
+}
+
+// goroutine pool
+type Pool struct {
+	sync.Mutex     //互斥锁
+	sync.WaitGroup //计数器,goroutine的个数
+
+	count int64                           //当前个数
+	capa  int64                           //pool cap
+	run   func() (interface{}, error)     //任务运行的func
+	quit  func(*Pool, interface{}, error) //退出func
+}
+
+func NewPool(capa int,
+	run func() (interface{}, error),
+	quit func(*Pool, interface{}, error)) *Pool {
+	if capa <= 0 || run == nil {
+		return nil
+	}
+	return &Pool{sync.Mutex{}, sync.WaitGroup{}, 0, int64(capa), run, quit}
+}
+
+func FromRunner(capa int, runner Runner) *Pool {
+	return NewPool(capa, runner.Run, runner.Quit)
+}
+
+// count may great then capa when user AddExecutor
+func (this *Pool) Count() int {
+	return int(atomic.LoadInt64(&this.count))
+}
+
+// return after all goroutine quit in pool
+//等待所有的goroutine退出
+func (this *Pool) WaitAllQuit() {
+	this.Wait()
+}
+
+//自动保持active状态
+func (this *Pool) AutoKeepalive(duration time.Duration) {
+	go this.autoKeepalive(duration)
+}
+
+func (this *Pool) autoKeepalive(duration time.Duration) {
+	defer common.CheckPanic()
+
+	for {
+		this.Keepalive()
+		time.Sleep(duration)
+	}
+}
+
+// check alive
+//检查pool是否是激活状态
+func (this *Pool) Keepalive() int {
+	defer common.CheckPanic()
+	this.Lock()
+	defer this.Unlock()
+
+	sub := int(this.capa - atomic.LoadInt64(&this.count))
+	for i := 0; i < sub; i++ {
+		this.AddExecutor()
+	}
+
+	return sub
+}
+
+// 手动创建pool执行器
+// add goroutine manual
+func (this *Pool) AddExecutor() {
+	defer common.CheckPanic()
+	wg := sync.WaitGroup{}
+	wg.Add(1)
+	go this.executor(&wg)
+	wg.Wait()
+}
+
+//创建goroutine list执行器
+//create this pool executor to run all run tasks
+func (this *Pool) executor(wg *sync.WaitGroup) {
+	defer common.CheckPanic()
+	this.Add(1)
+	defer this.Done()
+	atomic.AddInt64(&this.count, 1)
+	defer atomic.AddInt64(&this.count, -1)
+	wg.Done()
+
+	result, err := this.run()
+	if this.quit != nil {
+		this.quit(this, result, err)
+	}
+}
