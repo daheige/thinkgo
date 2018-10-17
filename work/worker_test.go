9611e90d338b7cd1aa256bb74ec9ddfe669027ce
daheige <zhuwei313@hotmail.com>
2018-10-17 23:14:39 +0800

    runner和work单元测试

--- work/worker_test.go
+++ work/worker_test.go
@@ -7,6 +7,18 @@ import (
 	"testing"
 )
 
+type myName struct {
+	name  string
+	age   int
+	index int
+}
+
+//实现了Worker接口
+func (this *myName) Task() {
+	log.Println("current index", this.index)
+	log.Println("your name is ", this.name)
+}
+
 type NamePrinter struct {
 	name  string
 	index int
@@ -66,3 +78,60 @@ user	0m0.395s
 sys	0m0.082s
 
 */
+
+/**
+$ go test -v -test.run TestPool
+--- PASS: TestPool2 (0.20s)
+PASS
+ok      thinkgo/work    0.379s
+*/
+func TestPool(t *testing.T) {
+	p := New(10) //使用10个goroutine 来创建工作池
+	var wg sync.WaitGroup
+
+	var taskNum = 10000
+	wg.Add(taskNum)
+	for i := 0; i < taskNum; i++ {
+		runtime.Gosched() //让出控制权给其他的goroutine
+		u := myName{
+			name:  "daheige",
+			age:   28,
+			index: i,
+		}
+
+		//异步的方式把任务提交到p中并运行
+		// goroutine相互竞争,通过异步的方式将任务提交到p.pool中
+		go func() {
+			//一旦工作池里的 goroutine 接收到这个值
+			// Run 方法就会返回。这也会导致 goroutine 将 WaitGroup 的计数递减,并终止 goroutine。
+			p.Run(&u)
+			wg.Done()
+		}()
+	}
+	wg.Wait() //一旦wg计数器减到0了,就要执行p.Shutdown关闭所有的通道,关闭工作池
+
+	// 让工作池停止工作,等待所有现有的工作完成
+	p.Shutdown()
+}
+
+/**
+$ go test -v -test.run TestPool2
+--- PASS: TestPool2 (0.17s)
+PASS
+ok      thinkgo/work    1.808s
+*/
+//无缓冲通道的同步执行任务
+func TestPool2(t *testing.T) {
+	p := New(10) //开辟10个goroutine来运行任务
+	for i := 0; i < 100000; i++ {
+		u := myName{
+			name:  "daheige",
+			age:   28,
+			index: i + 1,
+		}
+
+		p.Run(&u) //发送任务到goroutine池中，就会立即调用Task()执行
+	}
+
+	p.Shutdown()
+}
