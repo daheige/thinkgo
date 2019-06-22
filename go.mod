36818d38b479e96facb786bdee0b4f3a44026e98
daheige <zhuwei313@hotmail.com>
2019-06-22 17:42:28 +0800

    remove jsoniter and update logger

--- go.mod
+++ go.mod
@@ -6,13 +6,10 @@ require (
 	github.com/bitly/go-simplejson v0.5.0 // indirect
 	github.com/bmizerany/assert v0.0.0-20160611221934-b7ed37b82869 // indirect
 	github.com/go-sql-driver/mysql v1.4.1
-	github.com/go-xorm/xorm v0.7.1
+	github.com/go-xorm/xorm v0.7.3
 	github.com/go-yaml/yaml v2.1.0+incompatible
 	github.com/gomodule/redigo v2.0.0+incompatible
 	github.com/jinzhu/gorm v1.9.9
-	github.com/json-iterator/go v1.1.6
-	github.com/modern-go/concurrent v0.0.0-20180306012644-bacd9c7ef1dd // indirect
-	github.com/modern-go/reflect2 v1.0.1 // indirect
 	github.com/mozillazg/request v0.8.0
 	github.com/natefinch/lumberjack v2.0.0+incompatible
 	github.com/satori/go.uuid v1.2.0
--- mytest/json_test.go
+++ mytest/json_test.go
@@ -2,21 +2,18 @@ package mytest
 
 import (
 	"encoding/json"
+	"log"
 	"testing"
-
-	myjson "github.com/daheige/thinkgo/internal/json"
 )
 
-//测试github.com/json-iterator/go
-//json解析和反解析效率
-/**
+/**json解析和反解析效率
 $ time go test -v -test.run TestJson
 PASS
-ok      mytest  0.220s
+ok      github.com/daheige/thinkgo/mytest       0.718s
 
-real    0m1.015s
-user    0m1.179s
-sys 0m0.154s
+real    0m1.798s
+user    0m1.492s
+sys     0m0.208s
 */
 func TestJson(t *testing.T) {
 	t.Log("start test json")
@@ -32,33 +29,6 @@ func TestJson(t *testing.T) {
 	for i := 0; i < 10000; i++ {
 		data["count"] = data["count"].(int) + i
 		json_str, _ := json.Marshal(data)
-		t.Log(string(json_str))
-	}
-}
-
-/**
-time go test -v test.run TestMyJson
-PASS
-ok      mytest  0.173s
-
-real    0m0.898s
-user    0m1.086s
-sys 0m0.160s
-*/
-func TestMyJson(t *testing.T) {
-	t.Log("start test json")
-	var data = map[string]interface{}{
-		"code":    "200",
-		"message": "ok",
-		"data": []string{
-			"golang", "php", "nodejs",
-		},
-		"count": 1,
-	}
-
-	for i := 0; i < 10000; i++ {
-		data["count"] = data["count"].(int) + i
-		json_str, _ := myjson.Marshal(data)
-		t.Log(string(json_str))
+		log.Println(string(json_str))
 	}
 }
