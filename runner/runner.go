9b3ba4ca171d50945706115a346a6a1511a460af
daheige <zhuwei313@hotmail.com>
2020-01-11 13:01:51 +0800

    func option rewrite runner

--- runner/runner.go
+++ runner/runner.go
@@ -1,12 +1,11 @@
-/**runner用于按照顺序，执行程序任务操作，可作为cron作业或定时任务
-runner 包可用于展示如何使用通道来监视程序的执行时间,如果程序运行时间太长,也可以
-用 runner 包来终止程序。当开发需要调度后台处理任务的程序的时候,这种模式会很有用。这
-个程序可能会作为 cron 作业执行,或者在基于定时任务的云环境(如 iron.io)里执行。
+/** Package runner 用于按照顺序，执行程序任务操作，可作为cron作业或定时任务
+runner 包可用于展示如何使用通道来监视程序的执行时间,如果程序运行时间太长,指定任务执行时间。
+这个程序可能会作为 cron 作业执行,或者在基于定时任务的云环境(如 iron.io)里执行。
 补充说明：
 可能作为cron作业或基于定时任务,可以控制程序执行时间
 使用通道来监控程序的执行时间，生命周期，甚至终止程序等。
-我们这个程序叫runner，我们可以称之为执行者，它可以在后台执行任何任务
-而且我们还可以控制这个执行者，比如强制终止它等
+我们这个程序叫runner，我们可以称之为执行者。
+它可以在后台执行任何任务，而且我们还可以控制这个执行者，比如强制终止它等
 此外这个执行者也是一个很不错的模式，比如我们写好之后，交给定时任务去执行即可
 比如cron，这个模式我们还可以扩展更高效率的并发，更多灵活的控制程序的生命周期
 更高效的监控等。
@@ -15,108 +14,173 @@ package runner
 
 import (
 	"errors"
+	"fmt"
+	"log"
 	"os"
 	"os/signal"
 	"syscall"
 	"time"
 )
 
-var ErrorTimeout = errors.New("received timeout")
-var ErrInterrupt = errors.New("received interrupt signal")
+var (
+	ErrorTimeout = errors.New("task exec timeout")
+	ErrInterrupt = errors.New("received interrupt signal")
+)
 
-//声明一个runner
+type logger interface {
+	Println(msg ...interface{})
+}
+
+// Runner 声明一个runner
 type Runner struct {
-	tasks      []func() error   //需要执行的任务func,依次执行的任务
-	complete   chan error       //报告处理任务已完成
-	timeout    <-chan time.Time //所有任务多久可以执行完毕，只能接受通道中的值
-	interrupt  chan os.Signal   //可以控制强制终止的信号
-	allErrors  map[int]error    //发生错误的task index对应的错误
-	lastTaskId int              //最后一次完成的任务id
+	complete   chan error       // 有缓冲通道，存放所有任务运行后的结果状态
+	tasks      []func() error   // 执行的任务func,如果func没有错误返回，可以返回nil
+	timeout    time.Duration    // 所有的任务超时时间
+	timeCh     <-chan time.Time // 任务超时通道
+	logger     logger           // 日志输出实例
+	interrupt  chan os.Signal   // 可以控制强制终止的信号
+	allErrors  map[int]error    // 发生错误的task index对应的错误
+	lastTaskId int              // 最后一次完成的任务id
 }
 
-//定义一个工厂函数创建runner
-func New(t time.Duration) *Runner {
-	return &Runner{
-		complete:  make(chan error, 1),     //有缓冲通道，存放所有任务运行后的结果状态
-		timeout:   time.After(t),           //time.After返回time.Time类型的通道
-		interrupt: make(chan os.Signal, 1), //声明一个中断信号
-	}
-}
+// Option 采用func Option功能模式为Runner添加参数
+type Option func(r *Runner)
 
-func NewWithoutTime() *Runner {
-	return &Runner{
+// New 定义一个工厂函数创建runner
+// 默认创建一个无超时任务的runner
+func New(opts ...Option) *Runner {
+	r := &Runner{
 		complete:  make(chan error, 1),
 		interrupt: make(chan os.Signal, 1), //声明一个中断信号
 	}
+
+	// 初始化option
+	for _, o := range opts {
+		o(r)
+	}
+
+	if r.logger == nil {
+		r.logger = log.New(os.Stdout, "", log.LstdFlags)
+	}
+
+	return r
 }
 
-//将需要执行的任务添加到runner中
+// WithTimeout 设置任务超时时间
+func WithTimeout(t time.Duration) Option {
+	return func(r *Runner) {
+		r.timeout = t
+	}
+}
+
+// WithLogger 设置r.logger打印日志的句柄
+func WithLogger(l logger) Option {
+	return func(r *Runner) {
+		r.logger = l
+	}
+}
+
+// Add 将需要执行的任务添加到r.tasks队列中
 func (r *Runner) Add(tasks ...func() error) {
-	r.tasks = append(r.tasks, tasks...) //r.tasks是一个函数切片类型
+	r.tasks = append(r.tasks, tasks...)
 }
 
-//获取已经完成的任务ids
-func (r *Runner) ErrorTaskIds() map[int]error {
+// run 运行一个个任务,如果出错就返回错误信息
+func (r *Runner) run() (err error) {
+	for k, task := range r.tasks {
+		r.lastTaskId = k
+
+		if r.isInterrupt() {
+			err = ErrInterrupt
+			return
+		}
+
+		r.logger.Println("current run task id: ", k)
+
+		err = r.doTask(task)
+		if err != nil {
+			r.logger.Println("current task exec occur error: ", err)
+			r.allErrors[k] = err
+		}
+	}
+
+	return
+}
+
+// doTask 执行每个task，需要捕获每个任务是否出现了panic异常
+// 防止一些个别任务出现了panic,从而导致整个tasks执行全部退出
+func (r *Runner) doTask(task func() error) (err error) {
+	defer func() {
+		if e := recover(); e != nil {
+			r.logger.Println("current task throw panic: ", e)
+			err = fmt.Errorf("current task panic: %v", e)
+		}
+	}()
+
+	err = task()
+
+	return
+}
+
+// GetAllErrors 获取已经完成任务的error
+func (r *Runner) GetAllErrors() map[int]error {
 	return r.allErrors
 }
 
-//获取最后一次完成任务id
+// GetLastTaskId 获取最后一次完成任务id
 func (r *Runner) GetLastTaskId() int {
 	return r.lastTaskId
 }
 
-//运行一个个任务,如果出错就返回错误信息
-//run方法也很简单，会使用for循环，不停的运行任务，在运行的每个任务之前，都会检测是否收到了中断信号
-//如果没有收到，则继续执行,一直到执行完毕，返回nil；
-//如果收到了中断信号，则直接返回中断错误类型，任务执行终止
-func (r *Runner) Run() error {
-	for key, task := range r.tasks {
-		if r.isInterrupt() {
-			return ErrInterrupt
-		}
+// Start 开始执行所有的任务
+func (r *Runner) Start() error {
+	// 接收系统退出信号
+	signal.Notify(r.interrupt, syscall.SIGINT, syscall.SIGTERM, os.Interrupt, syscall.SIGHUP)
 
-		// log.Println("current run task id: ", key)
-		e := task() //运行任务
-		if e != nil {
-			r.allErrors[key] = e
-		}
+	r.allErrors = make(map[int]error, len(r.tasks)+1)
 
-		r.lastTaskId = key
+	if r.timeout > 0 {
+		r.timeCh = time.After(r.timeout)
 	}
 
-	return nil
-}
+	// 执行完毕的信号量
+	done := make(chan struct{}, 1)
 
-//开始执行所有的任务
-func (r *Runner) Start() error {
-	//接受runner中断或者程序中断信号
-	signal.Notify(r.interrupt, syscall.SIGINT, syscall.SIGTERM, os.Interrupt, syscall.SIGHUP)
-	//让任务在协程中运行，当运行接受后，将运行结果给complete
+	//开启独立goroutine执行任务
 	go func() {
-		r.complete <- r.Run() //运行返回错误或者nil顺利完成
+		defer func() {
+			if e := recover(); e != nil {
+				r.logger.Println("exec task panic: ", e)
+			}
+
+			close(done)
+		}()
+
+		r.complete <- r.run()
 	}()
 
-	// 判断哪个通道可操作
 	select {
-	case err := <-r.complete:
-		return err
-	case <-r.timeout: //超时，任务未执行完毕
+	case <-r.timeCh:
+		r.logger.Println(ErrorTimeout)
 		return ErrorTimeout
+	case <-done:
+		err := <-r.complete
+		r.logger.Println("task complete status: ", err)
+		return err
 	}
 }
 
-//检查是否接受到操作系统的中断信号
-//基于select的多路复用，select和switch很像，只不过它的每个case都是一个通信操作。
-//那么到底选择哪个case块执行呢？原则就是哪个case的通信操作可以执行就执行哪个，如果同时有多个可以执行的case，那么就随机选择一个执行。
-// 针对我们方法中，如果r.interrupt中接受不到值，就会执行default语句块，返回false
-// 一旦r.interrupt中可以接收值，就会通知Go Runtime停止接收中断信号，然后返回true。
+// isInterrupt 检查是否接受到操作系统的中断信号
+// 一旦r.interrupt中可以接收值，就会通知Go Runtime停止接收中断信号，然后返回true
 // 这里如果没有default的话，select是会阻塞的，直到r.interrupt可以接收值为止
 func (r *Runner) isInterrupt() bool {
 	select {
-	case <-r.interrupt:
+	case sg := <-r.interrupt: //是否接受到操作系统的中断信号
 		signal.Stop(r.interrupt)
+		r.logger.Println("received signal: ", sg.String())
+
 		return true
 	default:
-		return false //默认继续运行任务
+		return false
 	}
 }
--- runner/runner_test.go
+++ runner/runner_test.go
@@ -2,47 +2,55 @@ package runner
 
 import (
 	"log"
+	"os"
 	"testing"
 	"time"
 )
 
+// TestRunner test runner
 func TestRunner(t *testing.T) {
-	log.Println("======开始执行任务=======")
-	// timeout := 10 * time.Second //任务超时为3s
-	// r := New(timeout)           //创建一个runner
-	r := NewWithoutTime()
-	for i := 0; i < 100; i++ {
-		r.Add(createTask(i))
+
+	// 日志句柄
+	std := log.New(os.Stdout, "[runner] ", log.LstdFlags)
+
+	// New参数可选，默认创建无超时的任务
+	// p := New()
+
+	// p := New(WithLogger(std))
+
+	p := New(WithTimeout(3000*time.Millisecond), WithLogger(std))
+
+	for i := 0; i < 20000; i++ {
+		p.Add(createTask(i))
 	}
 
-	//开始执行任务
-	if err := r.Start(); err != nil {
-		switch err {
-		case ErrInterrupt: //中断
-			log.Println(err)
-		case ErrorTimeout: //超时
-			log.Println(err)
-		}
-	}
+	err := p.Start()
+	log.Println("error: ", err)
 
-	log.Println("任务执行完毕")
-	log.Println("错误的ids map: ", r.ErrorTaskIds())
-	log.Println("最后执行的task id: ", r.GetLastTaskId())
+	log.Println("last_id: ", p.GetLastTaskId())
+	log.Println("all error: ", p.GetAllErrors())
 }
 
+// createTask 创建任务
 func createTask(id int) func() error {
 	return func() error {
+		//panic(1)
+
 		log.Printf("正在执行任务%d", id)
-		time.Sleep(time.Duration(id) * time.Millisecond)
+		// time.Sleep(time.Duration(id) * time.Millisecond)
 		return nil
 	}
 }
 
-/**go test -v
-2019/04/15 22:15:42 任务执行完毕
-2019/04/15 22:15:42 错误的ids map:  map[]
-2019/04/15 22:15:42 最后执行的task id:  99
---- PASS: TestRunner (4.98s)
+/**
+[runner] 2020/01/11 12:55:17 current run task id:  2050
+2020/01/11 12:55:17 正在执行任务2050
+[runner] 2020/01/11 12:55:17 received signal:  interrupt
+[runner] 2020/01/11 12:55:17 task complete status:  received interrupt signal
+2020/01/11 12:55:17 error:  received interrupt signal
+2020/01/11 12:55:17 last_id:  2051
+2020/01/11 12:55:17 all error:  map[]
+--- PASS: TestRunner (1.16s)
 PASS
-ok  	github.com/daheige/thinkgo/runner	4.979s
+ok      github.com/daheige/thinkgo/runner       2.680s
 */
